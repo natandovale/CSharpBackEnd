98f99b11964c083a681712f4e4bf73ec104350d4
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-11-16 12:29:59 -0300

    Adicionando try catch ao metodo post em category controller

--- API-DataDrive/Shop/Shop/Controllers/CategoryController.cs
+++ API-DataDrive/Shop/Shop/Controllers/CategoryController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
 using Shop.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -26,16 +28,25 @@ namespace Shop.Controllers
 
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<Category>> Post([FromBody]Category model)
+        public async Task<ActionResult<Category>> Post([FromBody] Category model, [FromServices] DataContext context)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-            return Ok(model);
+            try
+            {
+                context.Categories.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch
+            {
+                return BadRequest(new { message = "Não foi possivel criar a categoria" });
+            }
         }
 
         [HttpPut]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> Put(int id,[FromBody]Category model)
+        public async Task<ActionResult<Category>> Put(int id, [FromBody] Category model)
         {
             if (id != model.Id)
                 return NotFound(new { message = "Categoria não encontrada" });
