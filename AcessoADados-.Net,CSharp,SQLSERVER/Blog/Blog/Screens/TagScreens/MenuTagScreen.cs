76953d7c4791661987f8e29e9ee766c619957b2b
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-11-29 15:18:06 -0300

    Renomeado pasta

---
+++ AcessoADados-.Net,CSharp,SQLSERVER/Blog/Blog/Screens/TagScreens/MenuTagScreen.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Screens.TagScreens
+{
+    public static class MenuTagScreen
+    {
+        public static void Load()
+        {
+            Clear();
+            WriteLine("Gestâo de tags");
+            WriteLine("--------------");
+            WriteLine("O que deseja fazer?");
+            WriteLine();
+            WriteLine("1 - Listar tags");
+            WriteLine("2 - Cadastrar tags");
+            WriteLine("3 - Atualizar tags");
+            WriteLine("4 - Excluir tags");
+            WriteLine();
+            WriteLine();
+            var option = short.Parse(ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    ListTagsScreen.Load();
+                    break;
+                case 2:
+                    CreateTagScreen.Load();
+                    break;
+                case 3:
+                    UpdateTagScreen.Load();
+                    break;
+                case 4:
+                    DeleteTagScreen.Load();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
