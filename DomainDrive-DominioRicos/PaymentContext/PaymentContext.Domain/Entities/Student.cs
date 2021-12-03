35c9afa9b0452431730da4e41b1e693a81b3d138
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-12-03 17:27:16 -0300

    Refatorando classe Student

---
+++ DomainDrive-DominioRicos/PaymentContext/PaymentContext.Domain/Entities/Student.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class Student
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Document { get; private set; }
+        public string Email { get; private set; }
+        public string Address { get; private set; }
+        public IReadOnlyCollection<Subscription> Subscriptions { get { return _subscriptions.ToArray(); } }
+        private IList<Subscription> _subscriptions { get; set; }
+
+        public Student(string firstName, string lastName, string document, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Document = document;
+            Email = email;
+            _subscriptions = new List<Subscription>();
+        }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            //Se já tiver uma assinatura ativa, cancela
+            //Cancela todas as outras assinaturas, e coloca esta como principal
+            foreach (var sub in Subscriptions)
+            {
+                sub.Active = false;
+            }
+            _subscriptions.Add(subscription);
+        }
+
+    }
+}
---
+++ DomainDrive-DominioRicos/PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var subscription = new Subscription();
+            var student = new Student("Andre","Baltiere","123456","hello@io.com");
+            student.AddSubscription(subscription);
+
+        }
+    }
+}
