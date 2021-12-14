1c10de920e4763939b91eb46b30f95652eb46523
natanaeloliveira <natanaeloliveira@mobills.com.br>
2021-12-14 17:51:35 -0300

    Configurando autenticacao e autorizacao da aplicaco

---
+++ API-DataDrive/Shop/Shop/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "g66@#e24#$343R354R#$#Rer";
+    }
+}
