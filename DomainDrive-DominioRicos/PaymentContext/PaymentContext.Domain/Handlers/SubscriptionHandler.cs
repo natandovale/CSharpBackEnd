b61c207a9151b6b5ed46d07d05b4af53714defea
natanaeloliveira <natanaeloliveira@mobills.com.br>
2022-03-08 18:01:19 -0300

    Criando handlers

---
+++ DomainDrive-DominioRicos/PaymentContext/PaymentContext.Domain/Handlers/SubscriptionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Flunt.Notifications;
+using PaymentContext.Domain.Commands;
+using PaymentContext.Shared.Commands;
+using PaymentContext.Shared.Handlers;
+
+namespace PaymentContext.Domain.Handlers
+{
+    class SubscriptionHandler : Notifiable, IHandler<CreateBoletoSubscriptionCommand>
+    {
+        public ICommandResult Handle(CreateBoletoSubscriptionCommand command)
+        {
+            return new CommandResult(true, "Assinatura realizada com sucesso");
+        }
+    }
+}
